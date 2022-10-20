1d19c4e41155c2174c2e157dc59f66954c28fee1
Laurent Ouma <lawiomosh3@gmail.com>
2022-10-20 16:31:25 +0300

    created the projects with name jujamall

--- jujamall/wsgi.py
+++ jujamall/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for life_eas project.
+WSGI config for jujamall project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'life_eas.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jujamall.settings')
 
 application = get_wsgi_application()
