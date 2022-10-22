b7fa469d1334957e2bf5ce0dc04ba5abc7e64913
Jeff Odhiambo <info@odhiambo-jeff.me>
2022-10-22 09:40:19 +0300

    contact page and about template done

--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+# A Class called Team and Contact
+# Team has a name, designation, photo, facebook link, twitter link, and a created date, email and phone number
+# Contact has a name, email, phone number, message, and a created date
+
+class Team(models.Model):
+    user = models.OneToOneField('auth.User',on_delete=models.CASCADE)
+    designation = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='team/')
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return self.user.username
+
+class Profile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profile/')
+    phone = models.CharField(max_length=200)
+    facebook_link = models.URLField(max_length=200)
+    twitter_link = models.URLField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.user.username
+
+class Contact(models.Model):
+    pass
--- core/views.py
+++ core/views.py
@@ -1,17 +1,27 @@
+from turtle import title
 from django.shortcuts import render
 from shop.models import BusinessProfile, BusinessBranch
 # Create your views here.
 def index(request):
     bus = BusinessBranch.objects.all()
-
+    title = 'home'
     context ={
-        'bus':bus
+        'bus':bus,
+        'title':title,
     }
-
+    #print(f"\n\n{title}\n\n")
     return render(request, 'index.html', context)
 
 def about(request):
-    return render(request, 'about.html')
+    title = 'About Us'
+    context = {
+        'title':title,
+    }
+    return render(request, 'about.html', context)
 
 def contact(request):
-    return render(request, 'contact.html')
+    title = 'Contact us'
+    context = {
+        'title':title
+    }
+    return render(request, 'contact.html', context)
