82ea206544e83d4db3de2fd708ad4995c1ff4fc2
Laurent Ouma <lawiomosh3@gmail.com>
2022-10-22 17:42:06 +0300

    team profile refactored to extende the profile

--- core/models.py
+++ core/models.py
@@ -2,11 +2,13 @@ from django.db import models
 import os
 from phonenumber_field.modelfields import PhoneNumberField
 
-
 # Create your models here.
 # A Class called Team and Contact
 # Team has a name, designation, photo, facebook link, twitter link, and a created date, email and phone number
 # Contact has a name, email, phone number, message, and a created date
+from account.models import Profile
+
+
 def content_file_name(instance, filename):
     ext = filename.split('.')[-1]
     filename = "%s_%s.%s" % (instance.user.id, instance.user.first_name, ext)
@@ -14,23 +16,15 @@ def content_file_name(instance, filename):
 
 
 class Team(models.Model):
-    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name='team')
     designation = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='team/')
+    photo = models.ImageField(upload_to=content_file_name)
     created_date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.user.username
-
-
-class Profile(models.Model):
-    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
-    profile_pic = models.ImageField(upload_to=content_file_name, blank=True, null=True)
-    phone = models.CharField(max_length=200, null=True, blank=True)
+    # added
     facebook_link = models.URLField(max_length=200, null=True, blank=True)
     twitter_link = models.URLField(max_length=200, null=True, blank=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.user.username
 
 
