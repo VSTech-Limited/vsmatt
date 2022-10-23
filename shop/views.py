10f84c927e8ce0dcbaf619742fe67f7d716862e6
Laurent Ouma <lawiomosh3@gmail.com>
2022-10-23 12:18:35 +0300

    Captures user current location when button clicked, otherwise the default is vstec limited location

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import BusinessProfile, BusinessBranch, Category
 
@@ -11,4 +11,8 @@ def business(request, slug=None):
         pass
     categories = Category.objects.all()
     bus = BusinessBranch.objects.all()
-    return render(request, 'shop/business.html', {'bus': bus,'categories': categories})
+    return render(request, 'shop/business.html', {'bus': bus, 'categories': categories})
+
+
+def add_business_location(request, slug):
+    category = get_object_or_404(Category, slug=slug)
