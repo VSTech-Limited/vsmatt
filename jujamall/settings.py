b83bfcf8f7d20906ed10ba2a62c3ef810544ed23
Jeff Odhiambo <info@odhiambo-jeff.me>
2022-10-29 20:19:27 +0300

    done

--- jujamall/settings.py
+++ jujamall/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'jujamall.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
