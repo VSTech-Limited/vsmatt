0f064161104cf2072980854fda872390fcc592f2
Jeff Odhiambo <info@odhiambo-jeff.me>
2022-10-29 18:18:59 +0300

    serving static using whitenoise

--- jujamall/settings.py
+++ jujamall/settings.py
@@ -153,8 +153,8 @@ USE_TZ = True
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'static'
+
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
 # ]
@@ -163,6 +163,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
