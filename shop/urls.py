75c7edf39b92fe59085470fc077f624bf8c63f05
Jeff Odhiambo <info@odhiambo-jeff.me>
2022-10-30 14:55:27 +0300

    done with linking on footer

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
-
 app_name = 'shop'
 urlpatterns = [
-    path('', views.business),
+    path('', views.business, name='business'),
     path('categories/<slug:slug>/', views.business, name='product_list_by_category'),
     path('business/categories/<slug:slug>/', views.businesses, name='business_list_by_category'),
     path('business/', views.get_business, name='get'),
