038ef53358666d79059db00e12afb97f3917a5b5
Laurent Ouma <lawiomosh3@gmail.com>
2022-10-31 15:21:11 +0300

    links added to markers info window and when clicked opens another window

--- api/views.py
+++ api/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from shop.models import BusinessProfile
 from .serializers.busineness_by_cartegory_serialisers import BusinessCategorySerializer
 from .serializers.serializers import BusinessSerializer
-from core.models import BusinessCategory
+from core.models import BusinessCategory, ProductCategory
 
 
 # Create your views here.
@@ -22,9 +22,32 @@ class BusinessesView(APIView):
 
 
 class BusinessByCategoryView(APIView):
-    def get(self, request, category_slug=None):
+    def get(self, request, business_category_slug=None):
         categories = BusinessCategory.objects.all()
-        if category_slug:
-            categories = BusinessCategory.objects.filter(slug=category_slug)
+        if business_category_slug:
+            categories = BusinessCategory.objects.filter(slug=business_category_slug)
+        serializer = BusinessCategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+
+# needs improvement after the database schema has been updated
+class BusinessByProductCategoryView(APIView):
+    def get(self, request, product_category_slug=None):
+        if not product_category_slug:
+            return redirect('categories')
+        product_category = get_object_or_404(ProductCategory, slug=product_category_slug)
+        # get list of products that belong to the category provided
+        category_products = product_category.product.all()
+        # get the branches where the each products got above are sold are sold
+        # [[branches for product 1], [branches for product 2], [branches for product n]]
+        branches_products = [category_product.branch.all() for category_product in category_products]
+        # go through branches groups to obtain distinct branches from different product to avoid
+        # repetition of branch for multiple product that belong to same category and are sold in
+        # one branch
+        branches = set()
+        for branch_list in branches_products:
+            branches.update(set(branch_list))
+        print(len(branches))
+        categories = BusinessCategory.objects.filter(businesses__in=branches)
         serializer = BusinessCategorySerializer(categories, many=True)
         return Response(serializer.data)
