fae210366baa27af40f06da1baef936647cb5ff3
Laurent Ouma <lawiomosh3@gmail.com>
2022-11-04 00:16:38 +0300

    Products add form added plus its view however doesnt persiste due to many to many field

--- products/views.py
+++ products/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+from django.utils.text import slugify
+
 from business.models import BusinessBranch
 from .forms import ProductsForm
 from .models import Product
@@ -20,7 +22,17 @@ def add_product(request, business_slug, branch_slug):
     branch = get_object_or_404(BusinessBranch, slug=branch_slug, business__owner=request.user,
                                business__slug=business_slug)
     form = ProductsForm()
-
+    if request.method == 'POST':
+        form = ProductsForm(
+            data=request.POST,
+            files=request.FILES
+        )
+        if form.is_valid():
+            new_product = form.save(commit=False)
+            new_product.branch = branch
+            new_product.save()
+            new_product.slug = f"{slugify(new_product.name)}-{new_product.id}"
+            new_product.save()
 
     return render(request, 'farm/products/add_products.html', {'branch': branch, 'form': form})
 
