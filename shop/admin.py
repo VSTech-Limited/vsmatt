95ca985f043d720be5ec2a0a6ace40be7d78f3e5
Laurent Ouma <lawiomosh3@gmail.com>
2022-11-06 13:30:06 +0300

    business contact registerd on admin pannel

--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from business.models import Contact
 
 
-
-
-
-
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'business', 'phone_number', 'message', 'is_addressed', 'created_at']
+    list_filter = ['is_addressed', 'created_at','business']
+    search_fields = ['name', 'email', 'phone_number', 'message']
+    date_hierarchy = 'created_at'
