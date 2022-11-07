13640ba7f01689257a2a7ba97cd43b5ece2af6fb
Jeff Odhiambo <odhiambo.jeff1@students.jkuat.ac.ke>
2022-11-07 09:56:50 +0300

    new changes

--- jujamall/settings.py
+++ jujamall/settings.py
@@ -197,3 +197,10 @@ ACCOUNT_USERNAME_REQUIRED = True
 # cart
 CART_SESSION_ID = 'cart'
 META_SESSION_ID = 'session_meta'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'physhamern1@gmail.com'
+EMAIL_HOST_PASSWORD = 'Lawiomosh3'
