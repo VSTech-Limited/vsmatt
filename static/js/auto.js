a9ca273ec2335e468df97a1f11a3501a720f5dd6
Laurent Ouma <lawiomosh3@gmail.com>
2022-11-08 11:42:13 +0300

    plotting search location on map anz zomming however the previous overlays not cleared and new marker not draggable

--- static/js/auto.js
+++ static/js/auto.js
@@ -1,32 +1,7 @@
 
-var places1 = [
-    "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua & Barbuda", "Argentina", "Armenia",
-    "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
-    "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia & Herzegovina", "Botswana", "Brazil", "British Virgin Islands",
-    "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands",
-    "Central Arfrican Republic", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire",
-    "Croatia", "Cuba", "Curacao", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
-    "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands",
-    "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia",
-    "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea",
-    "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran",
-    "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya",
-    "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya",
-    "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives",
-    "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco",
-    "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauro", "Nepal",
-    "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea",
-    "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines",
-    "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre & Miquelon",
-    "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone",
-    "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan",
-    "Spain", "Sri Lanka", "St Kitts & Nevis", "St Lucia", "St Vincent", "Sudan", "Suriname", "Swaziland", "Sweden",
-    "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga",
-    "Trinidad & Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks & Caicos", "Tuvalu", "Uganda", "Ukraine",
-    "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu",
-    "Vatican City", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"
-];
 var places = [];
+var coords = [];
+
 function aheadOfTimeSearch(country) {
     if (country.length > 2) {
         $.getJSON(
@@ -36,8 +11,10 @@ function aheadOfTimeSearch(country) {
                 places[country] = []
                 for (const result of results) {
                     const s = result['displayString']
-                    if(places.indexOf(s) === -1){
-                        places.push(s)
+                    const c = result['place']
+                    if (places.indexOf(s) === -1 && c) {
+                        places.push(s);
+                        coords.push(c)
                     }
                     /*
                     places[country].push({
@@ -54,15 +31,17 @@ function aheadOfTimeSearch(country) {
         );
     }
 }
-$(document).ready(function () {
-    //getData('/api/category/');
+
+
+
+function onReady() {
     $("#locationSearch").autocomplete({
         //called on input event
         source: (request, response) => {
             //works for now but inefficient, to be improved later
             const seachLocation = request.term;
             aheadOfTimeSearch(seachLocation)
-            const p = places.filter(function(place){
+            const p = places.filter(function (place) {
                 const lowerp = place.toLowerCase()
                 return lowerp.includes(seachLocation.toLowerCase())
             })
@@ -98,9 +77,34 @@ $(document).ready(function () {
         autofocus: true,
         // delay: 500,
     });
-});
+    
+}
 
+function onEnter(event, map) {
+    if (event.code == 'Enter') {
+        event.preventDefault();
+        const currIndex = places.indexOf($('#locationSearch').val())
+        if (currIndex !== -1) {
+            const lng = coords[currIndex]['geometry']['coordinates'][0]
+            const lat = coords[currIndex]['geometry']['coordinates'][1]
+            // alert(coords[currIndex]['geometry']['coordinates'])
+            // // alert(map)
+            const position = new google.maps.LatLng(lat, lng);
+            placeAndZoom(map, position)
+        }
+    }
+}
 
+function placeAndZoom(map, position) {
+    new google.maps.Marker({
+        position: position,
+        map: map,
+        animation: google.maps.Animation.BOUNCE,
+    });
+    map.setCenter(position);
+    map.setZoom(16);
+
+}
 
 
 
