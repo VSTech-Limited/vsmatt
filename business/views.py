909ae39adb85e200d136b19d696326d44699a24e
Jeff Odhiambo <jeff.odhiambo@studentambassadors.com>
2022-11-12 10:10:01 +0300

    added products to index page

--- business/views.py
+++ business/views.py
@@ -1,5 +1,4 @@
 from decimal import Decimal
-
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
